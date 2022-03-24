2c6c8b3935664ceee50bfe1f3df948345364ba26
AditiKachi <aditikachi24@gmail.com>
2022-03-24 16:32:52 +0530

    register.dart file is updated

--- lib/register.dart
+++ lib/register.dart
@@ -142,17 +142,3 @@ void ToastMessage(String message) => Fluttertoast.showToast(
     backgroundColor: Colors.grey,
     textColor: Colors.white,
     fontSize: 16.0);
-
-String? validatePassword(String value) {
-  RegExp regex =
-      RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
-  if (value.isEmpty) {
-    return 'Please enter password';
-  } else {
-    if (!regex.hasMatch(value)) {
-      return 'Enter valid password';
-    } else {
-      return null;
-    }
-  }
-}
